7830a9f359f8efe7273737001898641d312a2078
Alex Xavier <ajxv20@gmail.com>
2020-03-26 18:30:11 -0300

    Adc rota de Sessão e listagem de ONGs por perfil

--- backend/source/routes.js
+++ backend/source/routes.js
@@ -1,9 +1,15 @@
 const express = require('express')
 
+const SessionController = require('./controllers/SessionController')
+const ProfileController = require('./controllers/ProfileController')
 const OngController = require('./controllers/OngController')
 const IncidentsController = require('./controllers/IncidentsController')
 
 const routes = express.Router()
+
+routes.post('/sessions', SessionController.create)
+
+routes.get('/profile', ProfileController.index)
 
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create)
