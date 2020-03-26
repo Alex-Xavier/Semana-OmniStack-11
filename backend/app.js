06551a460561a1b9c44140ede1d01badcb3254aa
Alex Xavier <ajxv20@gmail.com>
2020-03-26 10:53:56 -0300

    Configurando nodemon e adicionando link de repo

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,11 @@ const express = require ('express')
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (request, response) => {
 	return response.json({
-		start: 'Hello World',
+		start: 'Hello World!!',
 		evento: 'Semana OmniStack 11.0',
 		aluno: 'Alex Xavier'
 	})
