f81f8df567442d8c17958eee071280114a9c4567
Alex Xavier <ajxv20@gmail.com>
2020-03-27 15:24:07 -0300

    Aprendendo sobre JSX + Propriedade + Imutabilidade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  function increment() {
+    setCounter(counter + 1)
+  }
+
   return (
-    <Header>
-      Semana OmniStack 11.0
-    </Header>
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
   )
 }
 
