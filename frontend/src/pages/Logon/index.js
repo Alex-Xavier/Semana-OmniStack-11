9bedc72de03cd10b37a4e13a8cc7e7b480d7cb7c
Alex Xavier <ajxv20@gmail.com>
2020-03-27 18:49:35 -0300

    Trocando linkagem HTML por um componente React

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FiLogIn } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 import './styles.css'
 import logoImg from '../../assets/logo.svg'
@@ -16,11 +17,10 @@ function Logon() {
 
 					<input placeholder="Sua ID"/>
 					<button className="button" type="submit">Entrar</button>
-
-					<a href="/register">
+					<Link to="/register">
 						<FiLogIn size={16} color="#E02041" />
 						Não tenho cadastro
-					</a>
+					</Link>
 				</form>
 			</section>
 
