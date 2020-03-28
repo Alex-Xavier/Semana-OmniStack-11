cdcf2f379b0d2eb69381d4dd3745f889d9d5b847
Alex Xavier <ajxv20@gmail.com>
2020-03-27 23:12:01 -0300

    Criação de rotas + Tela de Registro + Correções

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -3,6 +3,7 @@ import { FiLogIn } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 
 import './styles.css'
+
 import logoImg from '../../assets/logo.svg'
 import heroesImg from '../../assets/heroes.png'
 
@@ -17,7 +18,7 @@ function Logon() {
 
 					<input placeholder="Sua ID"/>
 					<button className="button" type="submit">Entrar</button>
-					<Link to="/register">
+					<Link className="back-link" to="/register">
 						<FiLogIn size={16} color="#E02041" />
 						Não tenho cadastro
 					</Link>
